ad947f51f20a9e1100010a1b8052382be10eac1e
Banksy <banksy@pomf.cat>
2016-10-09 17:36:25 -0500

    edits

---
+++ dist/donate.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Pomf.cat · FAQ</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="pomf.min.css">
+</head>
+<body>
+<div class="container">
+<div class="jumbotron">
+<header><h1>Donate</h1></header>
+<p class="lead">Bitcoin</p>
+</div>
+<ul>
+<li>Our servers aren't free! We try not to monetize our website for your privacy and we need donations to keep it running. If you'd like, donate to the Bitcoin address: <b>1MinRzq4A4WWHtqJvhcTNKBVPmtRdgCZ1w</b></li>
+</ul>
+<footer>
+        <?php
+                include "footer.php";
+        ?>
+</footer>
+</div>
+</body>
+</html>
